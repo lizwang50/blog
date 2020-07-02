d3646e5e3b375558df50761861569909f56a8d01
Liz Wang <erossatan33@gmail.com>
2020-07-02 18:50:24 +0800

    implement tailwind and vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="grid grid-rows-2 grid-flow-col gap-4">
+      <div class="row-span-3 flex-grow">
+        <Sidebar msg="Liz Wang" />
+      </div>
+      <div class="row-span-3 col-span-2">
+        <BlogTopPage msg="Welcome to Your Vue.js App"/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BlogTopPage from './components/BlogTopPage.vue'
+import Sidebar from './components/Sidebar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    BlogTopPage,
+    Sidebar
   }
 }
 </script>
 
 <style>
+@import "./assets/css/tailwind.css";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,45 @@
+<template>
+  <nav class="bg-secondary rounded-r-lg">
+    <div class="grid grid-flow-col grid-rows-6">
+      <div class="row-start-1 row-span-1 flex justify-center">
+        <img class="rounded-full" src="http://unsplash.it/74/74?gravity=center" alt="myPhoto" />
+      </div>
+      <div class="row-start-2 row-span-1 text-white">
+        <p class="text-white">{{ msg }}</p>
+      </div>
+      <div class="row-start-3 row-span-1 text-white">
+        <ul>
+          <li>
+            <a href="https://router.vuejs.org" target="_blank" rel="noopener">Blog</a>
+          </li>
+          <li>
+            <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">Project</a>
+          </li>
+          <li>
+            <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">Who's Liz?</a>
+          </li>
+          <li>
+            <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">Contact Liz</a>
+          </li>
+        </ul>
+      </div>
+      <div class="row-start-4 row-span-1">
+        <p class="text-white">copy right</p>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "SideBar",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
